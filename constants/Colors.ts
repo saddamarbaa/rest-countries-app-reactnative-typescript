3a3c9da607cd0d35fee96b911b640a277018fcd3
saddamarbaa <saddam@rushowl.sg>
2022-06-11 09:55:06 +0700

    chore: setup navigation

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,25 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = '#2f95dc'
+const tintColorDark = '#fff'
 
 export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
-};
+	light: {
+		text: '#000',
+		background: '#fff',
+		tint: tintColorLight,
+		tabIconDefault: '#ccc',
+		tabIconSelected: tintColorLight,
+	},
+	dark: {
+		text: '#fff',
+		background: '#000',
+		tint: tintColorDark,
+		tabIconDefault: '#ccc',
+		tabIconSelected: tintColorDark,
+	},
+}
+
+export const GlobalScreenOption = {
+	headerStyle: { backgroundColor: '#FAFAFA' },
+	headerTitleStyle: { color: '#111827' },
+	headerTintColor: '#111827',
+}
--- types.tsx
+++ types.tsx
@@ -3,33 +3,63 @@
  * https://reactnavigation.org/docs/typescript/
  */
 
-import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
-import { CompositeScreenProps, NavigatorScreenParams } from '@react-navigation/native';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { BottomTabScreenProps } from '@react-navigation/bottom-tabs'
+import {
+	CompositeScreenProps,
+	NavigatorScreenParams,
+} from '@react-navigation/native'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
 declare global {
-  namespace ReactNavigation {
-    interface RootParamList extends RootStackParamList {}
-  }
+	namespace ReactNavigation {
+		interface RootParamList extends RootStackParamList {}
+	}
 }
 
 export type RootStackParamList = {
-  Root: NavigatorScreenParams<RootTabParamList> | undefined;
-  Modal: undefined;
-  NotFound: undefined;
-};
+	Root: NavigatorScreenParams<RootTabParamList> | undefined
+	Modal: undefined
+	NotFound: undefined
+}
 
-export type RootStackScreenProps<Screen extends keyof RootStackParamList> = NativeStackScreenProps<
-  RootStackParamList,
-  Screen
->;
+export type RootStackScreenProps<Screen extends keyof RootStackParamList> =
+	NativeStackScreenProps<RootStackParamList, Screen>
 
 export type RootTabParamList = {
-  TabOne: undefined;
-  TabTwo: undefined;
-};
+	Detail: { name: string }
+	Home: undefined
+}
 
-export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
-  BottomTabScreenProps<RootTabParamList, Screen>,
-  NativeStackScreenProps<RootStackParamList>
->;
+export type RootTabScreenProps<Screen extends keyof RootTabParamList> =
+	CompositeScreenProps<
+		BottomTabScreenProps<RootTabParamList, Screen>,
+		NativeStackScreenProps<RootStackParamList>
+	>
+
+export type CountryType = {
+	name: {
+		official: string
+		nativeName?: {
+			ara: {
+				official: string
+			}
+		}
+	}
+	region: string
+	population: number
+	capital: string
+	flags: {
+		png: string
+	}
+	languages?: any
+	currencies?: any
+	subregion?: string
+	borders?: string[]
+	capitalInfo: {
+		latlng: [number, number]
+	}
+	maps: {
+		googleMaps: string
+		openStreetMaps: string
+	}
+}
